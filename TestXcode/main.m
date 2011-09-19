cb847e3f1f15841f66a7192e384d034d4597f934
Serge Gagnon <sergegagnon@me.com>
2011-09-19 13:57:36 -0400

    Initial Commit

---
+++ TestXcode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestXcode
+//
+//  Created by Gagnon Serge on 11-09-19.
+//  Copyright 2011 Maniko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
