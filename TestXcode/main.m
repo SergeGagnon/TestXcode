1e71d9e32932bdbcea452dde6a6f8954fbbaebe6
Serge Gagnon <sergegagnon@me.com>
2011-09-19 15:32:51 -0400

    Write first class

--- TestXcode/main.m
+++ TestXcode/main.m
@@ -8,14 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
+//---- @interface section ----
+
+@interface Fraction : NSObject {
+    int numerator;
+    int denominator;
+}
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+@end
+
+// ---- Implementation section ----
+
+@implementation Fraction
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int)n
+{
+    numerator = n;
+}
+
+-(void) setDenominator:(int)d
+{
+    denominator = d;
+}
+
+@end
+
+
+// ---- Program section ----
+
 int main (int argc, const char * argv[])
 {
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // comment here
-    // Adding comment here
-    NSLog(@"Test GitHub with Xcode 4");
+    Fraction *myFraction;
+    
+    // Create instance of Fraction
+    myFraction = [Fraction alloc];
+    myFraction = [myFraction init];
+    
+    // Set fraction to 1/3
+    [myFraction setNumerator:1];
+    [myFraction setDenominator:3];
+    
+    // Display the fractoin using the print method
+    
+    NSLog(@"The value of myFraction is:");
+    [myFraction print];
+    [myFraction release];
 
     [pool drain];
     return 0;
